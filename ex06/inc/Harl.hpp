f40d32c5794015dffeea210feca08ea4a5893af6
Alexis LOUBIERE <alex.loubiere@gmail.com>
2023-12-15 10:34:32 +0100

    added header and missing files

--- ex06/inc/Harl.hpp
+++ ex06/inc/Harl.hpp
@@ -1,4 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aloubier <alex.loubiere@42.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/15 10:31:28 by aloubier          #+#    #+#             */
+/*   Updated: 2023/12/15 10:32:33 by aloubier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
 # include <iostream>
+# include <string>
 
 #define RESET   "\033[0m"
 #define RED     "\033[31m"      /* Red */
@@ -19,3 +35,5 @@ private:
 	void	error(void);
 	
 };
+
+#endif
